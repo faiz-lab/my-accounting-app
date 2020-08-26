e6eea0eef76538ec14915d9ee040b0b4ba139f76
faiz-lab <qyp555@gmail.com>
2020-08-26 13:01:54 +0800

    init

---
+++ precache-manifest.4a7408c940d32e06406843f8f739c3a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afffce321092aaaf8e56c9390e8c9d8d",
+    "url": "/my-accounting-app/index.html"
+  },
+  {
+    "revision": "490a1810c3d3503b0619",
+    "url": "/my-accounting-app/static/css/main.47b4e45b.chunk.css"
+  },
+  {
+    "revision": "8e0cd5d0c488891e7840",
+    "url": "/my-accounting-app/static/js/2.2a1acb25.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/my-accounting-app/static/js/2.2a1acb25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "490a1810c3d3503b0619",
+    "url": "/my-accounting-app/static/js/main.59ae4d7e.chunk.js"
+  },
+  {
+    "revision": "7bb604cdf2311f8d2d2f",
+    "url": "/my-accounting-app/static/js/runtime-main.8a83a648.js"
+  }
+]);
